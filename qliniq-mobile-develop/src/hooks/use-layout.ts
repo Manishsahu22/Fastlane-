0f04f6d4a7225df9e64cebeb03fdd47078339963
Manishsahu22 <manish.sahu@dhinex.io>
2026-08-05 15:11:43 +0530

    Delete qliniq-mobile-develop directory

--- qliniq-mobile-develop/src/hooks/use-layout.ts
+++
@@ -1,8 +0,0 @@
-import { useWindowDimensions } from 'react-native';
-
-export function useLayout() {
-  const { width, height } = useWindowDimensions();
-  const isTablet = width >= 700;
-  const isLandscape = width > height;
-  return { width, height, isTablet, isLandscape };
-}
